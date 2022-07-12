01ae75aa4f9023c04be28e8a84212b046a9b9c6f
AtaruMatsudaira <matsu222212@gmail.com>
2022-07-12 23:43:51 +0900

    First commit

---
+++ app.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+datas = {}
+
+
+@app.route('/')
+def index():
+    return 'hello, world'
+
+
+@app.route("/AudioUpload", methods=['POST'])
+def AudioUpload():
+    datas["audios"] = request.form["audios"]
+    return make_response(jsonify({'result': True}))
+
+
+@app.route("/AudioDownload", methods=['GET'])
+def AudioDownload():
+    return make_response(datas["audios"])
+
+
+@ app.route("/ImageDownload", methods=['GET'])
+def ImageDownload():
+    return datas["image"]
+
+
+@ app.route("/ImageUpload/", methods=['GET', 'POST'])
+def ImageUpload():
+    datas["image"] = request.form["images"]
+    return make_response(jsonify({'result': True}))
+
+
+if __name__ == '__main__':
+    app.run()
